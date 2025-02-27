c66f27e556d564bd5cb42d9c9bef4ef694fc65ed
ViktorBl734 <harbourprogress@gmail.com>
2025-02-27 19:36:47 +0200

    Washing Station Task

--- app/main.py
+++ app/main.py
@@ -15,20 +15,26 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round((car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating)/self.distance_from_city_center, 1)
+        clean_power_diff = self.clean_power - car.clean_mark
+        cost_value = car.comfort_class * clean_power_diff * self.average_rating
+        return round(cost_value / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
-
-        new_average_rate = round((self.average_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1), 1)
+        new_average_rate = round(
+            (self.average_rating * self.count_of_ratings + rate)
+            / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
         self.average_rating = new_average_rate
 
-    def serve_cars(self,cars: list) -> float:
+    def serve_cars(self, cars: list) -> float:
+        total_income = 0.0
         for car in cars:
-            self.wash_single_car(car)
+            if car.clean_mark < self.clean_power:
+                total_income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
 
-        return sum([self.calculate_washing_price(car) for car in cars if car.clean_mark < self.clean_power])
+        return round(total_income, 1)
